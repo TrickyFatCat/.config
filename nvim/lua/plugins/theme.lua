e6aa4a766f4824b48f1fa6be0c157615fc8b6ac5
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-01-26 15:21:11 +0200

    Remove unused nvim themes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "Mofiqul/dracula.nvim",
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      diminactive = true,
-      theme = "dragon",
-    },
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
